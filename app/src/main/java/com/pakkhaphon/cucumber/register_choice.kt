14df8163cde4ad6af6cfac990459f4601c3de9de
pakkhaphon-j <pakkhaphon.j@gmail.com>
2022-01-28 01:48:50 +0700

    edit branch Key create and register condition

--- app/src/main/java/com/pakkhaphon/cucumber/register_choice.kt
+++ app/src/main/java/com/pakkhaphon/cucumber/register_choice.kt
@@ -12,11 +12,11 @@ import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 
-class register_choice : AppCompatActivity() {
+class register_choice: AppCompatActivity() {
 
     private lateinit var databaseref: DatabaseReference
     var id:Long = 0
-    private lateinit var mAuth:FirebaseAuth
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,7 +45,6 @@ class register_choice : AppCompatActivity() {
         val username = inputUsername.text.toString().trim()
         val email = inputEmail.text.toString().trim()
         val password = inputPassword.text.toString().trim()
-        val confirm = inputConfirm.text.toString().trim()
         var gender = ""
         var attention = ""
 
@@ -90,47 +89,52 @@ class register_choice : AppCompatActivity() {
             inputReceiver.setError("Please Select Attention")
         }
 
-
-        if((inputUsername.text.toString().isNotEmpty()) && (inputEmail.text.toString().isNotEmpty()) && (inputPassword.text.toString().isNotEmpty())) {
-            mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener {
-                    task -> if(task.isSuccessful) {
-                        databaseref = FirebaseDatabase.getInstance().reference.child("Users").child(mAuth.currentUser!!.uid)
-                        val data = HashMap<String,Any>()
-                        data["uid"] = mAuth.currentUser!!.uid
-                        data["image"] = "https://firebasestorage.googleapis.com/v0/b/cucumber-db.appspot.com/o/image%2Fperson-icon.png?alt=media&token=70867895-3dce-42c1-ad78-2778d3a08dd1"
-                        data["username"] = username
-                        data["password"] = password
-                        data["email"] = email
-                        data["gender"] = gender
-                        data["attention"] = attention
-                        databaseref.updateChildren(data).addOnCompleteListener {
-                                task -> if(task.isSuccessful) {
-                                    startActivity(Intent(this@register_choice,MainActivity::class.java))
-                                    val userdata = FirebaseDatabase.getInstance().reference.child("Users").child(mAuth.currentUser!!.uid).child("Local")
-                                    val data = HashMap<String,Any>()
-                                    data["address"] = ""
-                                    data["area"] = ""
-                                    data["info"] = ""
-                                    data["home_image"] = "https://firebasestorage.googleapis.com/v0/b/cucumber-db.appspot.com/o/image%2Fhome-icon.jpg?alt=media&token=fa484be3-75e8-4ab5-b8de-dbe2a062068f"
-                                    data["home_image1"] = "https://firebasestorage.googleapis.com/v0/b/cucumber-db.appspot.com/o/image%2Fhome-icon.jpg?alt=media&token=fa484be3-75e8-4ab5-b8de-dbe2a062068f"
-                                    data["home_image2"] = "https://firebasestorage.googleapis.com/v0/b/cucumber-db.appspot.com/o/image%2Fhome-icon.jpg?alt=media&token=fa484be3-75e8-4ab5-b8de-dbe2a062068f"
-                                    data["province"] = ""
-                                    data["road"] = ""
-                                    userdata.setValue(data)
-                                    val petdata = FirebaseDatabase.getInstance().reference.child("Users").child(mAuth.currentUser!!.uid).child("Pet")
-                                    val data1 = HashMap<String,Any>()
-                                    data1["pet_profile"] = "https://firebasestorage.googleapis.com/v0/b/cucumber-db.appspot.com/o/image%2Fdog-icon.png?alt=media&token=d67c854c-09ae-4274-a893-2784348f821e"
-                                    data1["pet_image"] = "https://firebasestorage.googleapis.com/v0/b/cucumber-db.appspot.com/o/image%2Fdog-icon.png?alt=media&token=d67c854c-09ae-4274-a893-2784348f821e"
-                                    data1["pet_image1"] = "https://firebasestorage.googleapis.com/v0/b/cucumber-db.appspot.com/o/image%2Fdog-icon.png?alt=media&token=d67c854c-09ae-4274-a893-2784348f821e"
-                                    data1["pet_image2"] = "https://firebasestorage.googleapis.com/v0/b/cucumber-db.appspot.com/o/image%2Fdog-icon.png?alt=media&token=d67c854c-09ae-4274-a893-2784348f821e"
-                                    data1["info"] = ""
-                                    data1["petname"] = ""
-                                    data1["sex"] = ""
-                                    petdata.setValue(data1)
-                                }
+        if((inputUsername.text.toString().isNotEmpty()) &&
+            (inputEmail.text.toString().isNotEmpty()) &&
+            (inputPassword.text.toString().isNotEmpty()) &&
+            (inputPassword.text.toString() == inputConfirm.text.toString()) &&
+            (inputConfirm.text.toString().isNotEmpty()) &&
+            (inputGender.getCheckedRadioButtonId() != -1) &&
+            (inputAttention.getCheckedRadioButtonId() != -1)) {
+                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+                        task -> if(task.isSuccessful) {
+                            databaseref = FirebaseDatabase.getInstance().reference.child("Users").child(mAuth.currentUser!!.uid)
+                            val data = HashMap<String, Any>()
+                            data["uid"] = mAuth.currentUser!!.uid
+                            data["image"] = "https://firebasestorage.googleapis.com/v0/b/cucumber-db.appspot.com/o/image%2Fperson-icon.png?alt=media&token=70867895-3dce-42c1-ad78-2778d3a08dd1"
+                            data["username"] = username
+                            data["password"] = password
+                            data["email"] = email
+                            data["gender"] = gender
+                            data["attention"] = attention
+                            databaseref.updateChildren(data).addOnCompleteListener {
+                                    task -> if(task.isSuccessful) {
+                                        startActivity(Intent(this@register_choice,MainActivity::class.java))
+                                        val userdata = FirebaseDatabase.getInstance().reference.child("Users").child(mAuth.currentUser!!.uid).child("Local")
+                                        val data = HashMap<String, Any>()
+                                        data["address"] = ""
+                                        data["area"] = ""
+                                        data["info"] = ""
+                                        data["home_image1"] = "https://firebasestorage.googleapis.com/v0/b/cucumber-db.appspot.com/o/image%2Fhome-icon.jpg?alt=media&token=fa484be3-75e8-4ab5-b8de-dbe2a062068f"
+                                        data["home_image2"] = "https://firebasestorage.googleapis.com/v0/b/cucumber-db.appspot.com/o/image%2Fhome-icon.jpg?alt=media&token=fa484be3-75e8-4ab5-b8de-dbe2a062068f"
+                                        data["home_image3"] = "https://firebasestorage.googleapis.com/v0/b/cucumber-db.appspot.com/o/image%2Fhome-icon.jpg?alt=media&token=fa484be3-75e8-4ab5-b8de-dbe2a062068f"
+                                        data["province"] = ""
+                                        data["road"] = ""
+                                        userdata.setValue(data)
+                                        val petdata = FirebaseDatabase.getInstance().reference.child("Users").child(mAuth.currentUser!!.uid).child("Pet")
+                                        val data1 = HashMap<String, Any>()
+                                        data1["pet_profile"] = "https://firebasestorage.googleapis.com/v0/b/cucumber-db.appspot.com/o/image%2Fdog-icon.png?alt=media&token=d67c854c-09ae-4274-a893-2784348f821e"
+                                        data1["pet_image1"] = "https://firebasestorage.googleapis.com/v0/b/cucumber-db.appspot.com/o/image%2Fdog-icon.png?alt=media&token=d67c854c-09ae-4274-a893-2784348f821e"
+                                        data1["pet_image2"] = "https://firebasestorage.googleapis.com/v0/b/cucumber-db.appspot.com/o/image%2Fdog-icon.png?alt=media&token=d67c854c-09ae-4274-a893-2784348f821e"
+                                        data1["pet_image3"] = "https://firebasestorage.googleapis.com/v0/b/cucumber-db.appspot.com/o/image%2Fdog-icon.png?alt=media&token=d67c854c-09ae-4274-a893-2784348f821e"
+                                        data1["pet_info"] = ""
+                                        data1["pet_name"] = ""
+                                        data1["pet_sex"] = ""
+                                        petdata.setValue(data1)
+                                    }
+                            }
                         }
-                    }
-            }
+                }
         }
     }
 }
